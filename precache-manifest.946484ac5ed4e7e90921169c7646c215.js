f11179d745bc794dc474614a79af679f2393870f
DevAbas <abas.memmedov.94@gmail.com>
2018-10-06 15:25:32 +0400

    Updates

---
+++ precache-manifest.946484ac5ed4e7e90921169c7646c215.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8fdbf7579a38197298",
+    "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
+  },
+  {
+    "revision": "c78ddd374a65243787f6",
+    "url": "/contact-manager/static/js/main.c78ddd37.chunk.js"
+  },
+  {
+    "revision": "ab9b73e89d24daeba495",
+    "url": "/contact-manager/static/js/1.ab9b73e8.chunk.js"
+  },
+  {
+    "revision": "c78ddd374a65243787f6",
+    "url": "/contact-manager/static/css/main.38b806d5.chunk.css"
+  },
+  {
+    "revision": "ab9b73e89d24daeba495",
+    "url": "/contact-manager/static/css/1.9b811964.chunk.css"
+  },
+  {
+    "revision": "14e94d83465d3f43a798a15de8f0e79b",
+    "url": "/contact-manager/index.html"
+  }
+];
